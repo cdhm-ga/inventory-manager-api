5d61459d57502f8fe544ae4652df9022ae608c63
matthewjmontalto <matthewjmontalto@gmail.com>
2019-03-27 17:00:26 -0400

    Initial Development File Structure
    - create item.js for item model/scheme.
    - export itemSchema.
    
    - create item_routes.js for item routes.
    - require boilerplate modules for routes.

---
+++ app/models/item.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+/* ~~~~~~~~~~~~~~Inventory Model Begins Here~~~~~~~~~~~~~~~~~~~ */
+
+/* ~~~~~~~~~~~~~~Inventory Model Ends Here ~~~~~~~~~~~~~~~~~~~~ */
+
+module.exports = mongoose.model('Item', itemSchema)
