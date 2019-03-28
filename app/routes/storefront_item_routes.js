7b0f5be600e5c37de31d49ed20a208cdcf639031
matthewjmontalto <matthewjmontalto@gmail.com>
2019-03-28 18:03:29 -0400

    Add UPDATE route to storefrontItem
    - add UPDATE route to JS file

--- app/routes/storefront_item_routes.js
+++ app/routes/storefront_item_routes.js
@@ -55,4 +55,27 @@ router.post('/storefront-items', requireToken, (req, res, next) => {
     .catch(next)
 })
 
+// UPDATE
+// PATCH /examples/5a7db6c74d55bc51bdf39793
+router.patch('/storefront-items/:id', requireToken, removeBlanks, (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.storefrontItem.owner
+
+  StorefrontItem.findById(req.params.id)
+    .then(handle404)
+    .then(storefrontItem => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, storefrontItem)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return storefrontItem.update(req.body.storefrontItem)
+    })
+    // if that succeeded, return 204 and no JSON
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
 module.exports = router
